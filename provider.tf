560e5b33af1fa87a21bd0e3360eb50ba3c344c4d
simoncastellanos2025 <simon.c.azure@gmail.com>
2025-04-26 19:36:38 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"  # or your preferred version
+    }
+  }
+}
+
+# main.tf
 provider "azurerm" {
   features {}
   subscription_id = "c5941903-69ad-4ba1-b201-6f86d9a24226"
   client_id       = "03122896-ae63-471a-a602-2aabf042a881"
+  #client_secret   = "your-client-secret"
   tenant_id       = "ac5b3248-01a9-4eb0-bdf4-e89223653965"
+  #skip_provider_registration = true  # Add this line
 }
+
+
