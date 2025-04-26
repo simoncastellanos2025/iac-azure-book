b5afd12c8f16961ff9a73087a35e734e83fad390
simoncastellanos2025 <simon.c.azure@gmail.com>
2025-04-26 19:21:20 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,6 @@
 provider "azurerm" {
   features {}
+  subscription_id = "c5941903-69ad-4ba1-b201-6f86d9a24226"
+  client_id       = "03122896-ae63-471a-a602-2aabf042a881"
+  tenant_id       = "ac5b3248-01a9-4eb0-bdf4-e89223653965"
 }
