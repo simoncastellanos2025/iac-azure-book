06366c7e07433649138f1b6b7379f7e40f13c4c0
simoncastellanos2025 <simon.c.azure@gmail.com>
2025-04-26 20:13:33 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -14,7 +14,7 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
   address_prefixes       = ["10.0.10.0/24"]
-   tags = {
+  tags = {
     environment = "production"
     owner       = "your-team"
   }
